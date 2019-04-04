66082665fb78c713644dbd1e9a3c56e78bf88d3b
Maninder Kaur <741327@student.tor.loc>
2019-04-04 10:58:01 -0400

    msg

---
+++ file.js
@@ -0,0 +1,10 @@
+var user = {
+    first_name: "CSS",
+    last_name: "HTML",
+    age: 4,/*from w w  ww.java 2  .c  o m*/
+    website: "java2s.com"
+};
+
+for (key in user) {
+    console.log(key);
+}
